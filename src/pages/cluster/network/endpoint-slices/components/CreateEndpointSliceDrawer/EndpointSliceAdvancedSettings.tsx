48ae022f8f08de9864e0420f4904b2ad0d041b15
lanyulei <fdevops@163.com>
2026-06-17 23:59:07 +0800

    feat: 完善

--- src/pages/cluster/network/endpoint-slices/components/CreateEndpointSliceDrawer/EndpointSliceAdvancedSettings.tsx
+++ src/pages/cluster/network/endpoint-slices/components/CreateEndpointSliceDrawer/EndpointSliceAdvancedSettings.tsx
@@ -5,10 +5,7 @@ import {
 } from '@/components';
 
 const EndpointSliceAdvancedSettings = () => (
-  <ResourceFormSection
-    description="关联服务标签会根据基本信息自动输出，这里可补充额外标签和注解。"
-    title="元数据"
-  >
+  <ResourceFormSection title="元数据">
     <CollapsibleField
       description="为 EndpointSlice 资源添加自定义标签和注解。"
       title="添加元数据"
